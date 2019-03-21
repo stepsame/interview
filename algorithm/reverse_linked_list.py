c0e45cfb9d7574589a8d8c8f05c63757d620db8b
Bu Tong <butong@zhihu.com>
2019-03-21 15:54:58 +0800

    reverse linked list

---
+++ algorithm/reverse_linked_list.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Definition for singly-linked list.
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverse_list(self, head: ListNode) -> ListNode:
+        # 1->2->3->4->5->None
+        # 5->4->3->2->1->None
+        pre, cur = None, head
+
+        while cur:
+            next_node = cur.next
+            cur.next = pre
+            pre = cur
+            cur = next_node
+
+        return pre
+
+
