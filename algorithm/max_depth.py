f234d005b7b71b8ff97cdb6a9f1a7dfb446a4845
Bu Tong <butong@zhihu.com>
2019-04-09 17:26:39 +0800

    max depth

---
+++ algorithm/max_depth.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        else:
+            left = self.maxDepth(root.left)
+            right = self.maxDepth(root.right)
+            if left > right:
+                return left + 1
+            else:
+                return right + 1
