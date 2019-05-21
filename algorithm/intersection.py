cc3cfad32512cacdba214aa284b8daeb0d3d0ecf
Tong Bu <stepsame@decentfox.com>
2019-05-21 14:03:41 +0800

    intersection

--- algorithm/intersection.py
+++ algorithm/intersection.py
@@ -14,38 +14,8 @@ class Solution(object):
         :type head1, head1: ListNode
         :rtype: ListNode
         """
-        length_a = 0
-        cur = headA
-        while cur and cur.next:
-            length_a += 1
-            cur = cur.next
-
-        length_b = 0
-        cur = headB
-        while cur and cur.next:
-            length_b += 1
-            cur = cur.next
-
-        if length_a > length_b:
-            steps = length_a - length_b
-            curA = headA
-            while steps > 0:
-                steps -= 1
-                curA = curA.next
-            curB = headB
-        else:
-            steps = length_b - length_a
-            curB = headB
-            while steps > 0:
-                steps -= 1
-                curB = curB.next
-            curA = headA
-
-        while curA and curB:
-            if curA == curB:
-                return curA
-            else:
-                curA = curA.next
-                curB = curB.next
-        return None
-
+        curA, curB = headA, headB
+        while curA != curB:
+            curA = curA.next if curA is not None else headB
+            curB = curB.next if curB is not None else headA
+        return curA
