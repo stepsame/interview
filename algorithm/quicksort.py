4e94267785127cdad86358ff5fa1b204c9b903e5
Tong Bu <stepsame@decentfox.com>
2019-05-22 10:39:09 +0800

    quick sort

--- algorithm/quicksort.py
+++ algorithm/quicksort.py
@@ -3,21 +3,39 @@ import random
 
 
 def quick_sort(array):
-    if len(array) < 2:
-        return array
-    else:
-        pivot_index = 0  # 选择第一个元素作为 pivot
-        pivot = array[pivot_index]
-        less_part = [i for i in array[pivot_index + 1:] if i <= pivot]
-        great_part = [i for i in array[pivot_index + 1:] if i > pivot]
-        return quick_sort(less_part) + [pivot] + quick_sort(great_part)
+    quick_sort_helper(array, 0, len(array) - 1)
+
+def quick_sort_helper(array, begin, end):
+    if begin >= end:
+        return
+    split_point = partition(array, begin, end)
+    quick_sort_helper(array, begin, split_point - 1)
+    quick_sort_helper(array, split_point + 1, end)
+
+def partition(array, begin, end):
+    pivot_value = array[begin]
+    left, right = begin + 1, end
+    while True:
+        while left <= right and array[left] <= pivot_value:
+            left += 1
+        while left <= right and array[right] > pivot_value:
+            right -= 1
+        if left > right:
+            break
+        array[left], array[right] = array[right], array[left]
+    array[begin], array[right] = array[right], array[begin]
+
+    return right
+    
 
 
 def test():
     ll = [i for i in range(10)]
     random.shuffle(ll)
-    new_ll = quick_sort(ll)
-    assert new_ll == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print(ll)
+    quick_sort(ll)
+    print(ll)
+    assert ll == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 if __name__ == '__main__':
